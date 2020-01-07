6a66109e710e6706f575f2d5ac2b5d320060d497
Danilo Imparato <imparatodanilo@gmail.com>
2020-01-07 12:21:39 -0300

    base commit

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' @importFrom tibble tibble
+NULL
+
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 476 rows and 4 variables:
+#' \describe{
+#'   \item{\code{taxid}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{string_name}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{new_taxid}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{ncbi_name}}{character COLUMN_DESCRIPTION}
+#'}
+#' @source \url{http://somewhere.important.com/}
+"string_eukaryotes"
+
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A newick file with 335 tips and taxids:
+#' \describe{
+#'   \item{\code{edge}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{Nnode}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{tip.label}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{taxid}}{character COLUMN_DESCRIPTION}
+#'}
+#' @source \url{http://somewhere.important.com/}
+"timetree_newick"
