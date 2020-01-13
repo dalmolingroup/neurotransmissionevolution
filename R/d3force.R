8d8ca8316b48b735f7cb65dc3bca6a902e5f93df
Danilo Imparato <imparatodanilo@gmail.com>
2020-01-13 02:57:44 -0300

    progress to d3force()

---
+++ R/d3force.R
@@ -0,0 +1,32 @@
+#' @export
+d3force <- function(graph, layout = NULL){
+  if(is.matrix(layout)){
+    V(graph)$x <- layout[,1]
+    V(graph)$y <- layout[,2]
+  }
+
+  graph_json <- jsonlite::toJSON(list(
+    nodes = igraph::as_data_frame(graph, "vertices")
+    ,links = igraph::as_data_frame(graph, "edges"))
+  )
+
+  print(graph_json)
+
+  server <- function(input, output, session) {
+    session$sendCustomMessage(type = "dataTransferredFromServer", graph_json)
+
+    shiny::observeEvent(input$coordinates, {
+      if(!is.null(input$coordinates)) shiny::stopApp(input$coordinates)
+    })
+  }
+
+  # shiny::addResourcePath('vivagraph.min.js', 'inst/www/vivagraph.min.js')
+  # shiny::addResourcePath('multiselect.js', 'inst/www/multiselect.js')
+  # shiny::addResourcePath('index.js', 'inst/www/index.js')
+
+  shiny::addResourcePath("www", system.file("www", package="neurotransmission"))
+
+  layout <- shiny::runGadget(shiny::shinyApp(ui = shiny::htmlTemplate(system.file("www/index.html", package="neurotransmission")), server))
+
+  matrix(layout, ncol=2,byrow=T)
+}
