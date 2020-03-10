8609877e142a4c3e9528b4f8b05a0c4e4a3ff3b9
daniloimparato <imparatodanilo@gmail.com>
2020-03-10 18:50:30 -0300

    attempt at easier custom knit

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,32 @@ download_if_missing <- function(url, filename) {
     download.file(url, filename)
   }
 }
+
+#' @export
+custom_knit <- function(input, encoding) {
+  ezknitr::ezknit(input, out_dir = here::here("manuscripts"), fig_dir = "figs", keep_html = F)
+}
+
+custom_knit2 <- function(input, encoding) {
+  knitr::opts_chunk$set(fig.path = "figsAAA")
+  rmarkdown::render(
+    input,
+    encoding       = encoding,
+    output_dir     = here::here("manuscripts"),
+    output_format  = "md_document",
+    output_options = list(
+      variant  = "markdown_github",
+      df_print = "kable",
+      dev      = "pdf"
+    )
+    # fig_dir = "figs",
+    # keep_html = F
+  )
+}
+
+
+custom_knit3 <- function(input, encoding) {
+  knitr::opts_knit$set(base.dir = "../manuscripts");
+  knitr::opts_chunk$set(fig.path = "figsAAA");
+  knitr::opts_knit
+}
