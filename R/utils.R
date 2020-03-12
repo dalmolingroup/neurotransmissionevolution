5f00029914b356c483911bb70c220c1041eff3f0
daniloimparato <imparatodanilo@gmail.com>
2020-03-11 21:01:38 -0300

    simpler download util function

--- R/utils.R
+++ R/utils.R
@@ -5,10 +5,7 @@ linMap <- function(x, from, to) approxfun(range(x), c(from, to))(x)
 darken <- function(color, intensity=0.5) rgb(t(col2rgb(color)*(1-intensity)/255))
 
 #' @export
-download_if_missing <- function(url, filename) {
-  if(missing(filename)) {
-    filename <- basename(url)
-  }
+download_if_missing <- function(url, filename = basename(url)) {
   filename <- here::here("data-raw/download", filename)
   if (!file.exists(filename)) {
     download.file(url, filename)
